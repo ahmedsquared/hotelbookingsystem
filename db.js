cf1ec90cf520225fb0bf939f019e7850ad0cf295
Mathew Begg <mbegg@ryerson.ca>
2021-03-27 14:04:03 -0400

    some filtering functionality

--- db.js
+++ db.js
@@ -14,3 +14,49 @@ async function connect(){
     
     return db;
 }
+
+async function searchRooms(parameters) {
+    var conn = await connect();
+    filterObject = constructFilterObject(parameters);
+    var results = await conn.collection('hotelRooms').find(filterObject).toArray();
+    return results;
+}
+
+function constructFilterObject(parameters) {
+    console.log('\nparameters:\n', JSON.stringify(parameters, null, 2));
+    filterObject = {
+        beds: {$in: convertBedSize(parameters.bedSize)},
+        facesDirection: parameters.facesDirection === 'Any' ? {$exists: true} : parameters.facesDirection,
+        hasBalcony: convertHasBalcony(parameters.hasBalcony),
+    };
+    console.log('\nfilterObject:\n', JSON.stringify(filterObject, null, 2));
+    return filterObject;
+}
+
+/**Returns an array of acceptable bedsizes given a minimum size
+ * e.g. minSize='Queen' => ['Queen', 'King']
+ */
+function convertBedSize(minSize) {
+    var filter = [
+        'Twin',
+        'Double',
+        'Queen',
+        'King'
+    ];
+    return filter.slice(filter.indexOf(minSize));
+}
+
+function convertHasBalcony(hasBalcony) {
+    switch (hasBalcony) {
+        case ('Any'):
+            return {$exists: true};
+        case ('Yes'):
+            return true;
+        case ('No'):
+            return false;
+    }
+}
+
+module.exports = {
+    searchRooms
+}
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db');
+
+var results = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', results: null });
 });
 
 router.get('/search', function(req, res, next) {
-  res.render('searchView', { title: 'Express' })
+  res.render('searchView', { title: 'Search' })
 });
+
+router.post('/search', async function(req, res) {
+  results = await db.searchRooms(req.body);
+  res.render('searchView', { title: 'Search Results', results: results })
+})
 
 module.exports = router;
