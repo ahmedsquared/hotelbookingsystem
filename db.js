3b02db35d4167f57a8001f033fe7e2f0c060ccf2
coliwong <colin.wong92@gmail.com>
2021-03-27 14:17:26 -0400

    Connected front-end to database. Made unique username condition.

--- db.js
+++ db.js
@@ -1,16 +1,29 @@
+const { registerHelper } = require("hbs");
 var { MongoClient } = require("mongodb");
 
 var db = null;
 async function connect(){
     if(db == null){
-        var url = 'mongodb+srv://dbUser:Wf81kH5ELPmpAo1u@cluster0.gnrno.mongodb.net/cps888?retryWrites=true&w=majority';
+        var url = 'mongodb+srv://coliwong:3Vh0IaUalo9V0YRC@cluster0.u1riz.mongodb.net/cps888?retryWrites=true&w=majority';
         var options = {
             useUnifiedTopology: true,
         };
 
         var connection = await MongoClient.connect(url, options);
-        db = connection.db("cps888");
+        db = await connection.db("cps888");
     }
     
     return db;
 }
+
+async function payment_info(username) {
+    var conn = await connect();
+    //Store user if exist into existingUser var
+    var existingUser = await conn.collection('users').findOne({ username });
+    
+    if(existingUser == null) {
+        await conn.collection('users').insertOne({ username });
+    }
+}
+
+payment_info("colin");
