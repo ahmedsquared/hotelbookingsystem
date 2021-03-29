aefaacaea5c3ec7a5712dfd2a21407741d8f000e
coliwong <colin.wong92@gmail.com>
2021-03-29 11:03:23 -0400

    Created Payment url: localhost:3000/payment

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var db = require("../db"); //import database
 
+router.get('/payment', async function(req, res) {
+  res.render('payment', {title: 'Payment Summary'})
+})
 /* GET home page. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.render('payment', { title: 'Hotel Bookings' });
-});
+});*/
+
 
 module.exports = router;
