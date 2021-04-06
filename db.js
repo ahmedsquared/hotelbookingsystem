a54d567bb54fac336f23b913acf6d112e22b18e7
Ahmed Ahmed <awahmed@cs.ryerson.ca>
2021-04-05 22:38:11 -0400

    Added getCustomerRooms and cancelBooking

--- db.js
+++ db.js
@@ -28,7 +28,7 @@ async function register(username, password){
     var SALT_ROUNDS = 10;
     var passwordHash = await bcrypt.hash(password, SALT_ROUNDS);
 
-    await conn.collection('customers').insertOne({username, passwordHash});
+    await conn.collection('customers').insertOne({username, passwordHash, bookedRooms});
 }
 
 async function login(username, password){
@@ -46,25 +46,75 @@ async function login(username, password){
     }
 }
 
-async function addRoom(numberOfBeds, bedSize, roomSize, viewType, price){
+async function addRoom(roomId, numberOfBeds, bedSize, roomSize, viewType, price){
     var conn = await connect();
-    var roomId = 1;
     var roomExists = await conn.collection('hotelRooms').findOne({roomId});
     var isBooked = false;
-    var bookedBy = "";
 
     while (roomExists != null){
-        roomId++;
-        roomExists = await conn.collection('hotelRooms').findOne({roomId});
+        throw new Error('Room already exists.');
     }
 
-    await conn.collection('hotelRooms').insertOne({roomId, numberOfBeds, bedSize, roomSize, viewType, price, isBooked, bookedBy});
+    await conn.collection('hotelRooms').insertOne({roomId, numberOfBeds, bedSize, roomSize, viewType, price, isBooked});
+}
+
+async function getCustomerRooms(username){
+    var conn = await connect();
+    var user = await conn.collection('customers').findOne({username});
+    var roomArr = [];
+    var roomId = 0;
+
+    let i;
+    for(i = 0;i < user.bookedRooms.length;i++){
+        roomId = user.bookedRooms[i];
+        var room = await conn.collection('hotelRooms').findOne({roomId});
+        roomArr.push(room);
+    }
+    
+    return roomArr;
+}
+
+async function cancelBooking(username, roomId){
+    var conn = await connect();
+    var user = await conn.collection('customers').findOne({username});
+    var room = await conn.collection('hotelRooms').findOne({roomId});
+    var newBookedRooms = user.bookedRooms;
+
+    roomId = parseInt(roomId, 10);
+
+    await conn.collection('hotelRooms').updateOne(
+        {roomId},
+        {
+            $set: {
+                isBooked: false,
+            }
+        }
+    )
+
+    let i;
+    for(i=0;i<newBookedRooms.length;i++){
+        if(newBookedRooms[i] === roomId){
+            newBookedRooms.splice(i, 1);
+            break;
+        }
+    }
+
+    await conn.collection('customers').updateOne(
+        {username},
+        {
+            $set: {
+                bookedRooms: newBookedRooms,
+            }
+        }
+    )
 }
 
 module.exports = {
     url,
     login,
     register,
+    getCustomerRooms,
+    cancelBooking,
 };
 
 //login('Ahmed', "lol");
